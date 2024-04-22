d7befd61d273330a9a7c58cf65937c873fd64575
unknown <dawid.pios00@gmail.com>
2024-04-22 17:55:28 +0200

    Added prisma, postgres db, configuration

--- server.js
+++ server.js
@@ -1,11 +1,22 @@
-const express = require("express");
+import express from 'express'
+import { PrismaClient } from "@prisma/client";
 const app = express();
+app.use(express.json());
+
+const prisma = new PrismaClient()
+
 app.get("/", (req, res) => {
   res.json({message: "Udało się HEHEHEH m"});
 });
+
+app.post('/add', async (req, res) => {
+
+  const user = await prisma.user.create({data: req.body})
+
+  res.json(user)
+})
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
 
-module.exports = app
