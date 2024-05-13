341de4df1739ca3b972707c79123599c0ca89e9d
unknown <dawid.pios00@gmail.com>
2024-05-13 16:07:27 +0200

    Revert

---
+++ index.ts
@@ -0,0 +1,27 @@
+import express from 'express'
+<<<<<<<< HEAD:index.ts
+import user from './routes/user.ts'
+
+========
+import { PrismaClient } from "@prisma/client";
+>>>>>>>> parent of c1db6e4 (Prisma, user controllers, routes, ts configs):server.js
+const app = express();
+app.use(express.json());
+
+const prisma = new PrismaClient()
+
+app.get("/", (req, res) => {
+  res.json({message: "Udało się HEHEHEH m"});
+});
+
+app.post('/add', async (req, res) => {
+
+  const user = await prisma.user.create({data: req.body})
+
+  res.json(user)
+})
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
+
