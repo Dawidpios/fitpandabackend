addeb978c137b0d1b807f2355d94e011101ca939
unknown <dawid.pios00@gmail.com>
2024-05-13 14:44:15 +0200

    Precommit try

--- dist/lib/hashPassword.js
+++
@@ -1,6 +0,0 @@
-import bcrypt from 'bcrypt';
-export const hashPassword = async (value) => {
-    const hashedPassword = await bcrypt.hash(value, 16);
-    return hashedPassword;
-};
-//# sourceMappingURL=hashPassword.js.map
--- dist/routes/user.js
+++
@@ -1,7 +0,0 @@
-import express from 'express';
-import { addUser, getUser } from '../controllers/userControllers.js';
-const router = express.Router();
-router.post('/addUser', addUser);
-router.get('/getUser', getUser);
-export default router;
-//# sourceMappingURL=user.js.map
